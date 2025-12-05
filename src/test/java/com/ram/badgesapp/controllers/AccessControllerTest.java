79b39d079fb569216b5e13267bd272464d668755
Unknown-mff <etu.moulahid.adib@gmail.com>
2025-12-05 17:09:27 +0100

    first commit

--- src/test/java/com/ram/badgesapp/controllers/AccessControllerTest.java
+++ src/test/java/com/ram/badgesapp/controllers/AccessControllerTest.java
@@ -27,7 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-
+//testcom
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(AccessController.class)
 public class AccessControllerTest {
